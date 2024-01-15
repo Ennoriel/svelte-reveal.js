b17ddf6b2d669e51dc5402fa3f081a1b3608164f
Maxime Dupont <m.dupont103@gmail.com>
2024-01-15 07:47:41 +0100

    Add open graph attributes

--- src/routes/[...slug]/slides/01-SlideTitle.svelte
+++ src/routes/[...slug]/slides/01-SlideTitle.svelte
@@ -3,11 +3,19 @@
 </script>
 
 <Slide>
-	<img
-		src="https://static.slid.es/reveal/logo-v1/reveal-white-text.svg"
-		alt="reveal.js"
-		style="height: 180px; margin: 0 auto 4rem auto; background: transparent;"
-		class="demo-logo"
-	/>
-	<h3>The reveal.js Svelte wrapper library</h3>
+	<div>
+		<img src="/svelte-reveal.js.svg" width="160" alt="svelte logo" />
+		<h1 class="r-fit-text">svelte-reveal.js</h1>
+	</div>
+	<h3>The reveal.js Svelte wrapper library!</h3>
 </Slide>
+
+<style>
+	div {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 48px;
+		margin: 32px;
+	}
+</style>
